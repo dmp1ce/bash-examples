86a988ee02863aff9647a86f46fb9e6bff17b181
David Parrish <daveparrish@gmail.com>
2014-07-04 10:16:08 -0400

    Add a one line countdown

--- countdown.sh
+++ countdown.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-for i in {1..10}
+echo "Countdown from 3."
+for i in {1..3}
 do
-	printf "$((11-i)) "
+	printf "$((4-i)) "
 	sleep 1
 done
 echo ""
+
+# One liner
+echo "Countdown from 3 (with only one line of code)."
+for i in {1..3}; do printf "$((4-i)) "; sleep 1; done; echo ""
