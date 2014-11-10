5fd78a1f80e3a01ac90fe4392981076e7970d128
David Parrish <daveparrish@gmail.com>
2014-11-10 11:44:04 -0500

    Add a couple more examples for checking parameters of a bash script.

--- parameter/check_parameter.sh
+++ parameter/check_parameter.sh
@@ -6,3 +6,17 @@ if [ -z $1 ]; then
 else
   echo "This script found the parameter '$1'"
 fi
+
+# Check to see if the first parameter is equal to 'abc'.
+if [ $1 == "abc" ]; then
+  echo "The first parameter is equal to 'abc'."
+else
+  echo "The first parameter is NOT equal to 'abc'."
+fi
+
+# Both check for the first parameter and if it is equal to 'abc'.
+if [[ -n $1 && $1 == "abc" ]]; then
+  echo "The first parameter exists and it is equal to 'abc'."
+else
+  echo "The first parameter either doesn't exist or is not equal to 'abc'."
+fi
